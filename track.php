00b684c9e47962f009bb262d0db545013e4c32c8
Alaa Al-Maliki <alaa.almaliki@gmail.com>
2021-11-10 00:40:42 +0000

    Map to multi target files

--- track.php
+++ track.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 $map = include __DIR__ . '/file_map.php';
 
-foreach ($map as $source => $target) {
-    copy($source, $target);
+foreach ($map as $source => $targets) {
+    foreach ($targets as $target) {
+        copy($source, $target);
+    }
 }
