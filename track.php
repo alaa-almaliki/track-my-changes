81bc745ba80539cd424e527b93569ddca0477978
Alaa Al-Maliki <alaa.almaliki@gmail.com>
2021-11-10 00:45:23 +0000

    safe map

--- track.php
+++ track.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 $map = include __DIR__ . '/file_map.php';
 
 foreach ($map as $source => $targets) {
+    if (is_string($targets)) {
+        $targets = [$targets];
+    }
+
     foreach ($targets as $target) {
         copy($source, $target);
     }
